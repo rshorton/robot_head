8d6ef3b190f434f4efdc4a30c582122b3a8b0ae7
Scott Horton <horton.rscott@gmail.com>
2021-06-09 20:24:50 -0500

    Creat launch file

---
+++ launch/robot_head.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_head',
+            executable='vision',
+        ),
+        Node(
+            package='robot_head',
+            executable='tracker',
+        ),
+        Node(
+            package='robot_head',
+            executable='viewer',
+        )
+    ])
